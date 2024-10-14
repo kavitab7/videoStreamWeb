64428a746458e6fa98d20b20fb5af5c381fc66e1
kavitab7 <kavitabhosale060@gmail.com>
2024-10-14 21:02:48 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const connectDB = require('./dbConnection');
+
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+})
